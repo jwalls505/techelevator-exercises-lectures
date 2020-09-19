34711b0d4e705d2a00f9bf103a8531e5185b68c5
Jesse Walls <drummerjw505@yahoo.com>
2020-09-19 15:44:05 -0400

    Submitting Temp/Linear conversion calculators

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,36 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Linear conversion tool.");
+		System.out.println();
+		
+		System.out.println("Please enter a measurement: ");
+		String userInput = input.nextLine();
+		double measureInput = Double.parseDouble(userInput);
+		
+		System.out.println();
+		System.out.println("Is that in (m)eter, or (f)eet? ");
+		String measureType = input.nextLine();
+		double conversion = 0;
+				if(measureType.contentEquals("f")) {
+					conversion = (measureInput * 0.3048);
+					System.out.println((int)measureInput + "f is " + (int)conversion + "m");
+				} else {
+					measureType.contentEquals("m");
+					conversion = (measureInput * 3.2808399);
+					System.out.println((int)measureInput + "m is " + (int)conversion + "f");
+				}
+		
+		
+		
+		
+ 
 	}
 
 }
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/TempConvert.java
@@ -1,9 +1,39 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class TempConvert {
 
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		System.out.println(" Spacer's Choice Temperature Conversion Calculater!");
+		
+		System.out.print("Please enter a temperature: ");						
+		String userInput = input.nextLine();
+		double tempInput = Double.parseDouble(userInput);
+		
+		System.out.println();
+		System.out.print("Is the temperature in (C)elsius, or (F)ahrenheit?: ");
+		String tempType = input.nextLine();
+		double conversion = 0;
+		if(tempType.contentEquals("F")) {
+			conversion = (tempInput - 32) / 1.8;
+			System.out.println((int)tempInput + tempType + " is " + (int)conversion + "C.");
+		} else {
+			tempType.contentEquals("C");
+			conversion = (tempInput * 1.8) + 32;
+			System.out.println((int)tempInput + tempType + " is " + (int)conversion + "F.");
+		}
+		 
+		
+		System.out.println();
+		System.out.println("Thank you for choosing Spacer's Choice!");
+		System.out.println("You've tried the best, now try the rest. Spacer's choice!");
+		
+		
+		
+		
+		
 	}
 
 }
