540154febe7a2fb66d4d1fac70e178f639c622ea
Jesse Walls <drummerjw505@yahoo.com>
2020-09-24 16:43:33 -0400

    Updated M1D8 lecture notes

--- module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
+++ module-1/08_Collections_Part_2/student-lecture/java/src/main/java/com/techelevator/Lecture.java
@@ -1,6 +1,7 @@
 package com.techelevator;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -17,7 +18,22 @@ public class Lecture {
 		breakOutGroup.put("David", 2);
 		breakOutGroup.put("Firdav", 3);
 		System.out.println(breakOutGroup);
+		Set<String> names = new HashSet<String>();
+		names.add("Dave");
+		names.add("Dan");
+		names.add("Elizabeth");
+		for(String name : names) {
+			System.out.println(name);
+		}
 		
+		
+		int sizeOfSet = names.size();
+		
+		boolean isEMptySet=names.isEmpty();
+		
+		names.remove("David");
+		
+		boolean hasDan = names.contains("Dan");
 	}
 
 }
