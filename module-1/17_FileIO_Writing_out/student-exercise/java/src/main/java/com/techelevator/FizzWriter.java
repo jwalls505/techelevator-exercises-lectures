be29e8b9c65b034cb162fee322e3b96c9c2402fd
Jesse Walls <drummerjw505@yahoo.com>
2020-10-07 08:28:33 -0400

    Submitting FizzWriter exercise.

--- module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,38 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class FizzWriter {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		String filePath = "FizzBuzz.txt";
+		File fizzFile = new File(filePath);
+		try {
+			fizzFile.createNewFile();
+		} catch(FileNotFoundException e) {
+			System.out.println("Something's not working.");
+		}
+		try (FileWriter fizzWriter = new FileWriter(fizzFile.getAbsoluteFile(), false);
+			PrintWriter file = new PrintWriter(fizzFile)) {
+			int counter = 0;
+			for (int i = 1; i <= 300; i++) {
+				if (i % 5 == 0 && i % 3 == 0) {
+					file.println("FizzBuzz");
+				} else if (i % 5 == 0) {
+					file.println("Buzz");
+				} else if (i % 3 == 0) {
+					file.println("Fizz");
+				} else {
+					counter++;
+					file.println(counter);
+				}
+			}
+		}
+		
+		System.out.println("The program has created FizzBuzz.txt. Thank you, come again.");
 	}
-
 }
