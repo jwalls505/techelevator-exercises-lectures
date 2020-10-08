5e07ad651acffb03143a8777a47a9c8924daafac
Jesse Walls <drummerjw505@yahoo.com>
2020-10-08 08:45:12 -0400

    work in progress challenge questions

--- module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/QuizMaker.java
@@ -1,10 +1,34 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class QuizMaker {
 
 	public static void main(String[] args) {
+		File inputFile = getInputFileFromUser();
 		
+		
+	
 
 	}
 
+	@SuppressWarnings("resource")
+	private static File getInputFileFromUser() {
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter path to input file >>> ");
+		String path = userInput.nextLine();
+		System.out.println();
+
+		File inputFile = new File(path);
+		if (inputFile.exists() == false) { // checks for the existence of a file
+			System.out.println(path + " does not exist");
+			System.exit(1); // Ends the program
+		} else if (inputFile.isFile() == false) {
+			System.out.println(path + " is not a file");
+			System.exit(1); // Ends the program
+		}
+		return inputFile;
+	}
 }
+
--- module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FileSplitter.java
+++ module-1/17_FileIO_Writing_out/student-exercise/java/src/main/java/com/techelevator/FileSplitter.java
@@ -1,9 +1,48 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileSplitter {
+	static Scanner userInput = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
+		File fileInput = getInputFromUser();
+		try (Scanner fileScanner = new Scanner(fileInput); Scanner in = new Scanner(System.in)) {
+			System.out.print("How many lines of text(max) should there be in the split files? >>> ");
+			int lineMax = in.nextInt();
+			long lineCounter = 0;
+			while (fileScanner.hasNextLine()) {
+				String line = fileScanner.nextLine();
+				lineCounter++;
+			}
+			System.out.println("The file has " + lineCounter + " lines.");
+			double temp = lineCounter/lineMax;
+			int fileCount;
+			
+		}
+
+	}
+
+	private static File getInputFromUser() {
+		System.out.print("What is the input file(please include the path-to-file? > ");
+		String path = userInput.nextLine();
+		System.out.println();
+
+		File inputFile = new File(path);
+		if (inputFile.exists() == false) { // checks for the existence of a file
+			System.out.println(path + " does not exist");
+			System.exit(1); // Ends the program
+		} else if (inputFile.isFile() == false) {
+			System.out.println(path + " is not a file");
+			System.exit(1); // Ends the program
+		}
+
+		return inputFile;
 	}
 
 }
