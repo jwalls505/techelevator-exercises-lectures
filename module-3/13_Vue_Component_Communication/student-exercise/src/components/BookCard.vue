d8afad0d13c216e67467cce3befd9720cf153c05
Jesse Walls <drummerjw505@yahoo.com>
2020-11-27 16:06:02 -0500

    M3D13 exerciese

--- module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
+++ module-3/13_Vue_Component_Communication/student-exercise/src/components/BookCard.vue
@@ -1,37 +1,65 @@
 <template>
-  <div class="card">
+  <div class="card" v-bind:class="{ read: book.read }">
+    <h2 class="book-title">
+      {{ book.title }}
+    </h2>
 
-    <!-- Please leave <img> commented out until directed to remove open and close comment tags in the README.
-    <img v-if="book.isbn" v-bind:src="'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'" />
-    -->
-    
+    <img
+      v-if="book.isbn"
+      v-bind:src="
+        'http://covers.openlibrary.org/b/isbn/' + book.isbn + '-M.jpg'
+      "
+    />
+
+    <h3 class="book-author">{{ book.author }}</h3>
+    <button
+      class="mark-unread"
+      v-on:click.prevent="markReadUnread"
+      v-if="book.read"
+    >
+      Mark Unread
+    </button>
+    <button
+      class="mark-read"
+      v-on:click.prevent="markReadUnread"
+      v-if="!book.read"
+    >
+      Mark Read
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'book-card'
-}
+  name: "book-card",
+  props: ["book"],
+  computed: {},
+  methods: {
+    markReadUnread() {
+      this.$store.commit("FLIP_STATUS", this.book);
+    },
+  },
+};
 </script>
 
 <style>
 .card {
-    border: 2px solid black;
-    border-radius: 10px;
-    width: 250px;
-    height: 550px;
-    margin: 20px;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 250px;
+  height: 550px;
+  margin: 20px;
 }
 
 .card.read {
-    background-color: lightgray;
+  background-color: lightgray;
 }
 
 .card .book-title {
-    font-size: 1.5rem;
+  font-size: 1.5rem;
 }
 
 .card .book-author {
-    font-size: 1rem;
+  font-size: 1rem;
 }
 </style>
---
+++ module-3/13_Vue_Component_Communication/student-exercise/src/components/NewBookForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form class="new-book-form" v-on:submit.prevent="addNewBook">
+      <label for="title-input">Title:</label>
+      <input class="title-input" v-model="newBook.title" />
+      <label for="author-input">Author:</label>
+      <input class="author-input" v-model="newBook.author" />
+      <label for="isbn-input">ISBN:</label>
+      <input class="isbn-input" v-modle="newBook.isbn" />
+      <input type="submit" value="Add Book" />
+      <input type="button" value="Reset" v-on:click.prevent="resetForm" />
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: "new-book-form",
+  data() {
+    return {
+      newBook: {
+        read: false,
+      },
+    };
+  },
+  methods: {
+    addNewBook() {
+      this.$store.commit("ADD_BOOK", this.newBook);
+    },
+    resetForm() {
+      this.newBook = {};
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
