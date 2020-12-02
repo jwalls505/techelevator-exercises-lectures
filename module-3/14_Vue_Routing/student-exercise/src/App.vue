e88f3dd2261a7c586a29b868624e17d7c294a622
Jesse Walls <drummerjw505@yahoo.com>
2020-12-02 09:22:26 -0500

    added Home, MyBooks,NewBookForm views

--- module-3/14_Vue_Routing/student-exercise/src/App.vue
+++ module-3/14_Vue_Routing/student-exercise/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
+    <nav>
+      <a href="/">Home |</a>&nbsp;
+      <a href="/myBooks">My Books</a>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app'
-}
+  name: "app",
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ module-3/14_Vue_Routing/student-exercise/src/views/MyBooks.vue
@@ -0,0 +1,12 @@
+<template>
+  <reading-list />
+</template>
+
+<script>
+import ReadingList from "@/components/ReadingList.vue";
+export default {
+  components: {
+    ReadingList,
+  },
+};
+</script>
