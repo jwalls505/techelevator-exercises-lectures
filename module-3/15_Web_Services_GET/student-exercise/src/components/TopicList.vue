7f919870df0bcaa91cc15ee49bc6753208a3f7b9
Jesse Walls <drummerjw505@yahoo.com>
2020-12-02 16:06:12 -0500

    Submitting M3D15 exercise

--- module-3/15_Web_Services_GET/student-exercise/src/components/TopicList.vue
+++ module-3/15_Web_Services_GET/student-exercise/src/components/TopicList.vue
@@ -2,19 +2,28 @@
   <div class="topic-list">
     <div v-for="topic in topics" v-bind:key="topic.id" class="topic">
       {{ topic.title }}
+      <router-link v-bind:to="{ name: 'Messages', params: { id: topic.id } }">{{
+        topic.title
+      }}</router-link>
     </div>
   </div>
 </template>
 
 <script>
+import topicService from "../services/topicService.js";
 export default {
-  name: 'topic-list',
+  name: "topic-list",
   data() {
     return {
-      topics: []
-    }
-  }
-}
+      topics: [],
+    };
+  },
+  created() {
+    topicService.getTopics().then((response) => {
+      this.topics = response.data;
+    });
+  },
+};
 </script>
 
 <style>
